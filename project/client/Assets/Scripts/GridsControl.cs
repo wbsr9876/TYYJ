d01134f3d5e91a001c4c23536654ff0d77e04d78
wbsr9876 <57223744wz@sina.com>
2014-03-30 04:47:57 +0800

    按格子拖拽的脚本&修改海绵宝宝缩放BUG
    暂时写死格子坐标，以后放个透明格子图或者什么的就可以直接读相应的坐标了。
    海绵宝宝在动画里配了强制缩放0.5啊，巨大啊...

---
+++ project/client/Assets/Scripts/GridsControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameBase;
+
+public class GridsControl : MonoBehaviour {
+
+	[HideInInspector]
+	public Grids m_grids;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//貌似是局部坐标系的问题，暂时不用这种替代方式，反正最后是要单独创建网格的
+		//BoxCollider2D test = this.GetComponent<BoxCollider2D>();
+		//Vector2 rectPos = test.center - test.size;
+		//m_grids = new Grids(new Rect(rectPos.x,rectPos.y,test.size.x*2,test.size.y*2),9,5);
+		m_grids = new Grids(new Rect(-7.85f,-4.65f,12.7f,8.3f),9,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ project/client/Assets/Scripts/MoveControl.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameBase;
+
+public class MoveControl : MonoBehaviour {
+
+	[HideInInspector]
+	private bool m_bDragging = false;
+	private Vector3 m_oldPos;
+
+	private Grids m_sceneGrids;
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject myBackground = GameObject.FindWithTag("background");
+		if(myBackground)
+		{
+			GridsControl gc = myBackground.GetComponent<GridsControl>();
+			m_sceneGrids = gc.m_grids;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_bDragging)
+		{
+			Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			newPos.z = 0;
+			transform.Translate(newPos - transform.position,Space.World);
+		}
+	}
+
+	void OnMouseDown()
+	{
+		m_oldPos = transform.position;
+		//print("Down Pos:" + m_oldPos);
+		m_bDragging = true;
+	}
+
+	void OnMouseUp()
+	{
+		m_bDragging = false;
+		Rect rect = m_sceneGrids.GetGrid(transform.position);
+		Vector3 newPos;
+		if(rect.width < 0.1)
+		{
+			newPos = m_oldPos;
+		}
+		else
+		{
+			newPos = new Vector3(rect.center.x,rect.center.y);
+		}
+		//print("Up Pos:" + m_oldPos);
+		transform.Translate(newPos - transform.position,Space.World);
+	}
+}
